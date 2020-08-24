93a498bccd73a180d202b7228ac58e643c130276
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-08-25 02:20:42 +0400

    Mon, 24 Aug 2020 22:20:41 +0000

--- readable/views/seo.py
+++ readable/views/seo.py
@@ -10,7 +10,7 @@ from readable.utils.decorators import x_robots_tag
 @method_decorator(x_robots_tag, name="dispatch")
 class RobotsView(TemplateView):
     content_type = "text/plain"
-    template_name = "robots.txt"
+    template_name = "seo/robots.txt"
 
     @property
     def _allowed(self) -> List[str]:
@@ -36,4 +36,24 @@ class RobotsView(TemplateView):
         return context
 
 
+@method_decorator(x_robots_tag, name="dispatch")
+class SitemapView(TemplateView):
+    content_type = "application/xml"
+    template_name = "seo/sitemap.xml"
+
+    @property
+    def _urls(self) -> List[str]:
+        return ["index", "login", "registration"]
+
+    def _locations(self, urls: List[str]) -> List[str]:
+        reversed_urls: Iterable[str] = map(reverse_lazy, urls)
+        return list(map(self.request.build_absolute_uri, reversed_urls))
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super(SitemapView, self).get_context_data(**kwargs)
+        context["locations"] = self._locations(self._urls)
+        return context
+
+
 robots_view = RobotsView.as_view()
+sitemap_view = SitemapView.as_view()
