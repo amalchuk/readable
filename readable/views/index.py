4aa759debffd44db2f93c2a313e71c9cdddeaa7d
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-10-28 19:35:49 +0400

    Wed, 28 Oct 2020 15:35:49 +0000

--- readable/views/index.py
+++ readable/views/index.py
@@ -3,7 +3,6 @@ from typing import Any, Dict
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.urls.base import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -12,15 +11,12 @@ from django.views.generic.edit import CreateView
 from readable.forms import DocumentsForm
 
 
+@method_decorator(login_required, name="post")
 class IndexView(CreateView):
     form_class = DocumentsForm
     http_method_names = ["get", "post"]
     success_url = reverse_lazy("index")
     template_name = "index.html"
-
-    @method_decorator(login_required)
-    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
-        return super(IndexView, self).post(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         if self.request.user.is_authenticated:
