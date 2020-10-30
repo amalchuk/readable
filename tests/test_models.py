ab330684c70daa8a78e7bcaa824ca7124e2c7df6
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-10-30 22:57:28 +0400

    Fri, 30 Oct 2020 18:57:27 +0000

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,7 @@
-from io import BytesIO
-
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.db.models.signals import post_save
 from django.test.testcases import TestCase
-from docx import Document as DOCXDocument
 
 from readable.models import Documents
 from readable.models import Staff
@@ -13,23 +10,16 @@ from readable.utils.signals import documents_uploaded
 
 class TestDocuments(TestCase):
     def setUp(self) -> None:
-        document = DOCXDocument()
-        document.add_heading("Document Title", 0)
-        document.add_paragraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-
-        docx_document = BytesIO()
-        document.save(docx_document)
-        self.docx_document = ContentFile(docx_document.getvalue(), "document.docx")
-
+        self.lorem = ContentFile("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "lorem.txt")
         user = User.objects.create_user(username="staff", password="staff")
-        self.staff = Staff.objects.create(user=user)
+        self.staff = Staff.objects.create(user=user, user_agent="Mozilla/5.0", ip_address="127.0.0.1")
 
     def test_upload_directory(self) -> None:
         # Temporarily disable the "documents_uploaded" signal:
         post_save.disconnect(documents_uploaded, Documents)
 
-        document = Documents.objects.create(filename=self.docx_document, uploaded_by=self.staff)
-        self.assertEqual(document.realname, self.docx_document.name)
+        document = Documents.objects.create(filename=self.lorem, uploaded_by=self.staff)
+        self.assertEqual(document.realname, self.lorem.name)
         self.assertEqual(document.filename, f"{document.id!s}{document.path.suffix}")
 
         self.assertTrue(document.unavailable)
