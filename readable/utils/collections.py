a3242e55b5590edbc28bb0a5d11a93c7e16cd032
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-03-15 12:29:39 +0400

    Mon, 15 Mar 2021 08:29:39 +0000

--- readable/utils/collections.py
+++ readable/utils/collections.py
@@ -5,22 +5,23 @@ __all__: Final[List[str]] = ["as_iterable", "as_list", "as_tuple"]
 T = TypeVar("T")
 
 
-def as_iterable(obj: T, /) -> Iterator[T]:
+def as_iterable(obj: T, /, *args: T) -> Iterator[T]:
     """
     Return an ``iterable`` containing only the specified ``object``.
     """
     yield obj
+    yield from args
 
 
-def as_list(obj: T, /) -> List[T]:
+def as_list(obj: T, /, *args: T) -> List[T]:
     """
     Return a ``list`` containing only the specified ``object``.
     """
-    return list(as_iterable(obj))
+    return list(as_iterable(obj, *args))
 
 
-def as_tuple(obj: T, /) -> Tuple[T, ...]:
+def as_tuple(obj: T, /, *args: T) -> Tuple[T, ...]:
     """
     Return a ``tuple`` containing only the specified ``object``.
     """
-    return tuple(as_iterable(obj))
+    return tuple(as_iterable(obj, *args))
--- tests/test_collections.py
+++ tests/test_collections.py
@@ -12,11 +12,14 @@ class TestCollections(TestCase):
         sentinel = object()
         self.assertIsInstance(as_iterable(sentinel), Iterable)
         self.assertIn(sentinel, as_iterable(sentinel))
+        self.assertIn("true", as_iterable("true", "false"))
 
     def test_as_list(self) -> None:
         sentinel = object()
         self.assertListEqual([sentinel], as_list(sentinel))
+        self.assertListEqual([sentinel, sentinel], as_list(sentinel, sentinel))
 
     def test_as_tuple(self) -> None:
         sentinel = object()
         self.assertTupleEqual((sentinel,), as_tuple(sentinel))
+        self.assertTupleEqual((sentinel, sentinel), as_tuple(sentinel, sentinel))
