7e2fed24d4db3b37fc0c5f68ab6ea81983285b8e
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-03-16 07:24:11 +0400

    Tue, 16 Mar 2021 03:24:11 +0000

--- readable/utils/collections.py
+++ readable/utils/collections.py
@@ -1,27 +1,27 @@
-from typing import Final, Iterator, List, Tuple, TypeVar
+from typing import Final, FrozenSet, Iterator, List, Set, Tuple, TypeVar, Union
 
-__all__: Final[List[str]] = ["as_iterable", "as_list", "as_tuple"]
+__all__: Final[List[str]] = ["as_frozenset", "as_iterable", "as_list", "as_set", "as_tuple"]
 
-T = TypeVar("T")
-
-
-def as_iterable(obj: T, /, *args: T) -> Iterator[T]:
-    """
-    Return an ``iterable`` containing only the specified ``object``.
-    """
-    yield obj
-    yield from args
+_T = TypeVar("_T")
+_S = TypeVar("_S")
 
 
-def as_list(obj: T, /, *args: T) -> List[T]:
-    """
-    Return a ``list`` containing only the specified ``object``.
-    """
-    return list(as_iterable(obj, *args))
+def as_iterable(item: _T, /, *items: _S) -> Iterator[Union[_T, _S]]:
+    yield item
+    yield from items
 
 
-def as_tuple(obj: T, /, *args: T) -> Tuple[T, ...]:
-    """
-    Return a ``tuple`` containing only the specified ``object``.
-    """
-    return tuple(as_iterable(obj, *args))
+def as_tuple(item: _T, /, *items: _S) -> Tuple[Union[_T, _S], ...]:
+    return tuple(as_iterable(item, *items))
+
+
+def as_list(item: _T, /, *items: _S) -> List[Union[_T, _S]]:
+    return list(as_iterable(item, *items))
+
+
+def as_set(item: _T, /, *items: _S) -> Set[Union[_T, _S]]:
+    return set(as_iterable(item, *items))
+
+
+def as_frozenset(item: _T, /, *items: _S) -> FrozenSet[Union[_T, _S]]:
+    return frozenset(as_iterable(item, *items))
