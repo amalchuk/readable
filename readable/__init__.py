58e282f71772b600d015305861cafbedfd5d7002
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-04-07 09:08:29 +0400

    Wed, 07 Apr 2021 05:08:29 +0000

--- readable/__init__.py
+++ readable/__init__.py
@@ -4,9 +4,3 @@
 """
 Check and improve the spelling and grammar of documents.
 """
-
-from typing import Final
-
-__all__: Final[list[str]] = ["default_app_config"]
-
-default_app_config: str = "readable.apps.Configuration"
