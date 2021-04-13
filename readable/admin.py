210b3b8857d64c17e6c950626f2ce79efe3bd291
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-04-13 10:08:06 +0400

    Tue, 13 Apr 2021 06:08:38 +0000

--- readable/admin.py
+++ readable/admin.py
@@ -37,7 +37,7 @@ class StaffInline(StackedInline):
 
 @register(User)
 class UserAdmin(BaseUserAdmin):
-    def get_inlines(self, request: HttpRequest, obj: Optional[User]) -> list[type[InlineModelAdmin]]:
+    def get_inlines(self, request: HttpRequest, obj: Optional[User], /) -> list[type[InlineModelAdmin]]:
         return super(UserAdmin, self).get_inlines(request, obj) if obj is None else as_list(StaffInline)
 
 
@@ -70,22 +70,22 @@ class DocumentsAdmin(ModelAdmin):
         })
     ]
 
-    def has_change_permission(self, request: HttpRequest, obj: Optional[Documents] = None) -> bool:
+    def has_change_permission(self, request: HttpRequest, obj: Optional[Documents] = None, /) -> bool:
         return request.user.is_superuser and obj is not None and obj.unavailable
 
-    def has_delete_permission(self, request: HttpRequest, obj: Optional[Documents] = None) -> bool:
+    def has_delete_permission(self, request: HttpRequest, obj: Optional[Documents] = None, /) -> bool:
         return request.user.is_superuser and obj is not None and obj.unavailable
 
-    def get_fieldsets(self, request: HttpRequest, obj: Optional[Documents] = None) -> FieldSetsType:
+    def get_fieldsets(self, request: HttpRequest, obj: Optional[Documents] = None, /) -> FieldSetsType:
         return self.add_fieldsets if obj is None else super(DocumentsAdmin, self).get_fieldsets(request, obj)
 
-    def get_inlines(self, request: HttpRequest, obj: Optional[Documents]) -> list[type[InlineModelAdmin]]:
+    def get_inlines(self, request: HttpRequest, obj: Optional[Documents], /) -> list[type[InlineModelAdmin]]:
         return super(DocumentsAdmin, self).get_inlines(request, obj) if obj is None else as_list(MetricsInline)
 
-    def get_queryset(self, request: HttpRequest) -> "QuerySet[Documents]":
+    def get_queryset(self, request: HttpRequest, /) -> "QuerySet[Documents]":
         qs: "QuerySet[Documents]" = super(DocumentsAdmin, self).get_queryset(request)
         return qs if request.user.is_superuser else qs.filter(uploaded_by__user=request.user)
 
-    def save_model(self, request: HttpRequest, obj: Documents, *args: Any, **kwargs: Any) -> None:
+    def save_model(self, request: HttpRequest, obj: Documents, /, *args: Any, **kwargs: Any) -> None:
         obj.uploaded_by = request.user.staff
         obj.save()
